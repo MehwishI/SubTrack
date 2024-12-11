872fd604717c98e4ea24e6bceca2e6025164f435
Mehwish <mehwishned21@gmail.com>
2024-12-10 23:44:23 -0600

    Add user name in login

--- reactApp/subtract-react-app/src/components/Home/Home.js
+++ reactApp/subtract-react-app/src/components/Home/Home.js
@@ -1,7 +1,26 @@
 import React, { useEffect, useState } from "react";
+import Login from "../Authentication/Login";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Home = () => {
-  return <div>Home page</div>;
+  const { user, isAuthenticated, isLoading, logout } = useAuth0();
+
+  const handleLoginClick = () => {
+    console.log("Login clicked!");
+    return <Login />;
+  };
+  const handleLogout = () => {
+    logout({ logoutParams: { returnTo: window.location.origin } });
+  };
+  console.log(isAuthenticated);
+  return (
+    <>
+      <div>Welcome {isAuthenticated && user.name} to SubTrack</div>
+      {!isAuthenticated && <button onClick={handleLoginClick}>Login</button>}
+
+      {isAuthenticated && <button onClick={handleLogout}>Logout</button>}
+    </>
+  );
 };
 
 export default Home;
