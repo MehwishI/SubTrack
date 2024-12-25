620558c17c19ab75a68d871c9312b48e6a095eab
Mehwish <mehwishned21@gmail.com>
2024-12-25 02:41:40 -0600

    work in progress

--- reactApp/subtrack/src/components/Authentication/Login.js
+++ reactApp/subtrack/src/components/Authentication/Login.js
@@ -2,7 +2,8 @@ import React, { useEffect } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useNavigate } from "react-router";
 import "./Login.css";
-
+import loginimg from "../images/login/image.png";
+import frame from "../images/login/Frame 4409.png";
 const Login = () => {
   const { loginWithRedirect, user } = useAuth0();
   const navigate = useNavigate();
@@ -19,15 +20,23 @@ const Login = () => {
 
   return (
     <div className="login-container">
-      <h2>SUBTrack Login</h2>
-      <button onClick={getHandleLogin}>Login</button>
-      <br></br>
-      <label
-        onClick={gethandleLabelClick}
-        style={{ cursor: "pointer", color: "blue" }}
-      >
-        Please signup here
-      </label>
+      <div>
+        <h2>SUBTrack Login</h2>
+        <button onClick={getHandleLogin}>Login</button>
+        <br></br>
+        <label
+          onClick={gethandleLabelClick}
+          style={{ cursor: "pointer", color: "blue" }}
+        >
+          Please signup here
+        </label>
+      </div>
+      <div className="login-img">
+        {/* <img src={loginimg} id="loginimg"></img> */}
+        <div className="login-frame">
+          <img src={frame} />
+        </div>
+      </div>
     </div>
   );
 };
