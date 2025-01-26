3044ea7811be072e98637e126157b26f551a7474
Mehwish <mehwishned21@gmail.com>
2025-01-25 18:32:43 -0600

    fixed deployemnt bugs

---
+++ expressServer/controllers/reminderController.js
@@ -0,0 +1,68 @@
+const subService = require("../services/notificationService");
+const {
+  getDecryptedData,
+  getEncrytedData,
+} = require("../services/encryptService");
+
+const fetchReminderData = async (req, res) => {
+  try {
+    const remData = await subService.getReminder(req.body.userid);
+    if (!remData) {
+      res.status(404).send("Reminder data not available for this user.");
+    } else {
+      return res.status(200).send(remData);
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const saveReminderData = async (req, res) => {
+  // start decrypt data
+  const getDecryptData = req.body;
+  //getDecryptedData(req.body);
+
+  //const userid = getDecryptData.userid;
+  const subid = getDecryptData.subid;
+
+  const remData = {
+
+  
+
+    freeTrial: getDecryptData.freeTrial,
+    paymentDue: getDecryptData.paymentDue,
+    paymentDuetom: getDecryptData.paymentDuetom,
+    active/inactive: getDecryptData.active/inactive,
+    spendSummary: getDecryptData.spendSummary,
+ 
+    mobilePush: getDecryptData.mobilePush,
+    desktopRem: getDecryptData.desktopRem,
+    emailRem: getDecryptData.emailRem,
+}
+
+  // const notData = {
+  //   subject: getDecryptData.subject,
+  //   notification_type: getDecryptData.notification_type,
+  //   frequency: getDecryptData.frequency,
+  //   status: getDecryptData.status,
+  //   date_sent: getDecryptData.date_sent,
+
+  //   message: getDecryptData.message,
+
+  //   //parking_date: Date.now(),
+  // };
+
+
+  try {
+    const savedRem = await remService.saveReminder(remData, subid);
+    if (savedRem) {
+      return res.status(200).send("Reminder data saved successfully!");
+    } else res.status(500).send("sub notification reminder data not saved!");
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error.message);
+  }
+};
+
+module.exports = { fetchReminderData, saveReminderData };
---
+++ expressServer/routes/reminderRoute.js
@@ -0,0 +1,100 @@
+//routes
+const express = require("express");
+const router = express.Router();
+const {
+  saveReminderData,
+  fetchReminderData,
+} = require("../controllers/reminderController");
+const { saveReminder } = require("../services/reminderService");
+
+/**
+ *@swagger
+ * /api/subscription/reminder:
+ *   post:
+ *     tags:
+ *       -  Subscription Reminder
+ *     summary: Returns  subscription reminder from DB.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               subid:
+ *                 type: string
+ *                 example: "type subscription id here"
+ *
+ *     responses:
+ *       200:
+ *         description: A successful response
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example:  Subscription Reminder data!
+ */
+
+/**
+ *@swagger
+ * /api/subscription/reminder/save:
+ *   post:
+ *     tags:
+ *       -  Subscription  Reminder
+ *     summary: Saves the  subscription reminder data into Database.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               subid:
+ *                  type: string
+ *                  example: "Add subscription id here"
+ *               freeTrial:
+ *                  type: boolean
+ *
+ *                  example: "Free Trial True or false"
+ *               paymentDue:
+ *                  type: boolean
+ *                  example: "Payment Due True or false"
+ *               paymentDuetom:
+ *                  type: boolean
+ *                  example: "payment due tomorrow , in 3 days"
+ *               active_inactive:
+ *                  type: boolean
+ *                  example: "active or inactive"
+ *               spendSummary:
+ *                  type: boolean
+ *                  example: "true or false"
+ *               mobilePush:
+ *                  type: boolean
+ *                  example: "mobile push true/false"
+ *               desktopPush:
+ *                  type: boolean
+ *                  example: "desktop Push true /false"
+ *               emailRem:
+ *                  type: boolean
+ *                  example: "email remider true / false"
+ 
+ *
+ *     responses:
+ *       200:
+ *         description: A successful response
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "Subscription reminder data saved successfully"
+ */
+
+router.post("/subscription/reminder", fetchReminderData);
+router.post("/subscription/reminder/save", saveReminderData);
+module.exports = router;
